a1394d67e5f784fa25f18ea036558f9331ca6a9b
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-09-22 19:50:22 -0600

    Ejercicio 4 de cuenta listo

---
+++ ejercicio4/index.js
@@ -0,0 +1,45 @@
+var Cuenta = /** @class */ (function () {
+    // Constructor
+    function Cuenta(nombre, cantidad, tipoDeCuenta, numeroDeCuenta) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.tipoDeCuenta = tipoDeCuenta;
+        this.numeroDeCuenta = numeroDeCuenta;
+    }
+    // metodo para depositar dinero
+    Cuenta.prototype.depositar = function (monto) {
+        if (monto < 5)
+            console.log('😢🐴 Mi rey usted es pobre? debe ingresar un monto mayor a $5.00. ✔🔃');
+        else {
+            this.cantidad += monto;
+            console.log("\uD83E\uDD11 Se ha depositado correctamente: $".concat(monto, "."));
+            console.log("\uD83D\uDCB2 Saldo actual: $".concat(this.cantidad, "."));
+        }
+    };
+    // metodo para retirar dinero
+    Cuenta.prototype.retirar = function (valor) {
+        if (valor < 5)
+            console.log('💱 La cantidad a retirar debe ser mayor de $5.00.');
+        else if (this.cantidad < valor)
+            console.log('😢 No hay suficiente saldo en la cuenta para retirar esa cantidad.');
+        else {
+            this.cantidad -= valor;
+            console.log("\uD83D\uDCB1 Has retirado: $".concat(valor, "."));
+            console.log("\uD83D\uDCB2 Saldo restante: $".concat(this.cantidad, "."));
+        }
+    };
+    // metodo para mostrar los datos de la cuenta
+    Cuenta.prototype.mostrarDatos = function () {
+        console.log("\uD83D\uDC34 Nombre: ".concat(this.nombre));
+        console.log("\uD83E\uDD16 Tipo de cuenta: ".concat(this.tipoDeCuenta));
+        console.log("\uD83C\uDF87 N\u00FAmero de cuenta: ".concat(this.numeroDeCuenta));
+    };
+    return Cuenta;
+}());
+// Ejemplo de uso
+var miCuenta = new Cuenta('John Doe', 100, 'Ahorros', '1234567890');
+miCuenta.mostrarDatos();
+miCuenta.depositar(50);
+miCuenta.retirar(30);
+miCuenta.retirar(200);
+miCuenta.depositar(3);
---
+++ ejercicio4/index.ts
@@ -0,0 +1,53 @@
+class Cuenta {
+    private nombre: string;
+    private cantidad: number;
+    private tipoDeCuenta: string;
+    private numeroDeCuenta: string;
+
+    // Constructor
+    constructor(nombre: string, cantidad: number, tipoDeCuenta: string, numeroDeCuenta: string) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.tipoDeCuenta = tipoDeCuenta;
+        this.numeroDeCuenta = numeroDeCuenta;
+    }
+
+    // metodo para depositar dinero
+    public depositar(monto: number): void {
+        if (monto < 5)
+            console.log('😢🐴 Mi rey usted es pobre? debe ingresar un monto mayor a $5.00. ✔🔃');
+        else {
+            this.cantidad += monto;
+            console.log(`🤑 Se ha depositado correctamente: $${monto}.`);
+            console.log(`💲 Saldo actual: $${this.cantidad}.`);
+        }
+    }
+
+    // metodo para retirar dinero
+    public retirar(valor: number): void {
+        if (valor < 5)
+            console.log('💱 La cantidad a retirar debe ser mayor de $5.00.');
+        else if (this.cantidad < valor)
+            console.log('😢 No hay suficiente saldo en la cuenta para retirar esa cantidad.');
+        else {
+            this.cantidad -= valor;
+            console.log(`💱 Has retirado: $${valor}.`);
+            console.log(`💲 Saldo restante: $${this.cantidad}.`);
+        }
+    }
+    // metodo para mostrar los datos de la cuenta
+    public mostrarDatos(): void {
+        console.log(`🐴 Nombre: ${this.nombre}`);
+        console.log(`🤖 Tipo de cuenta: ${this.tipoDeCuenta}`);
+        console.log(`🎇 Número de cuenta: ${this.numeroDeCuenta}`);
+    }
+}
+
+// Ejemplo de uso
+const miCuenta = new Cuenta('John Doe', 100, 'Ahorros', '1234567890');
+
+miCuenta.mostrarDatos();
+miCuenta.depositar(50);
+miCuenta.retirar(30);
+miCuenta.retirar(200);
+miCuenta.depositar(3);              
